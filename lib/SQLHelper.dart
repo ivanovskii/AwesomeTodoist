87da1bbce328bac426af008168c0ba8d12e61c60
Stas Ivanovskii <ivanovskiy999@gmail.com>
2022-06-09 12:20:47 +1000

    Add SQLite support

---
+++ lib/SQLHelper.dart
@@ -0,0 +1,53 @@
+import 'dart:async';
+import 'package:path/path.dart' show join;
+import 'package:awesome_todoist/TodoModel.dart';
+import 'package:sqflite/sqflite.dart';
+
+// Created using this tutorial:
+// https://pub.dev/packages/sqflite
+
+class SQLHelper {
+  Database? database;
+
+  void onCreate(Database db, int version) async {
+    return (await db.execute(
+        'CREATE TABLE Todo ('
+            'id INTEGER PRIMARY KEY,'
+            'description TEXT NOT NULL,'
+            'notifyAt TEXT,'
+            'done INTEGER)')
+    );
+  }
+
+  Future<Database> initDatabase() async {
+    String path = join(await getDatabasesPath(), "todo_db.db");
+    return await openDatabase(path, version: 1, onCreate: onCreate);
+  }
+
+  Future<Database?> get db async {
+    return database ?? await initDatabase();
+  }
+
+  Future<int?> insert(Todo todo) async {
+    var database = await db;
+    return await database!.insert('Todo', todo.toMap());
+  }
+
+  Future<Todo> getTodo(int id) async {
+    var database = await db;
+    var maps = await database!.rawQuery('SELECT * FROM Todo WHERE id = $id');
+    return Todo.fromMap(maps.first);
+  }
+
+  Future<List> all() async {
+    var database = await db;
+    return (await database!.rawQuery('SELECT * FROM Todo')).toList();
+  }
+
+  Future<int> delete(int id) async {
+    var database = await db;
+    return await database!.delete('Todo', where: '$id = ?', whereArgs: [id]);
+  }
+
+  Future close() async => database!.close();
+}
